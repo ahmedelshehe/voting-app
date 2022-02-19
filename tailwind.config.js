9954201f353b8b83abd9623e5356d70fc40cd0ab
Ahmed Ayman <ahmed.el-shehy@student.guc.edu.eg>
2022-02-20 01:36:11 +0200

    Adding Idea Form

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,5 +51,7 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+],
 };
